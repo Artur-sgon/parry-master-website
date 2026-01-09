0d2dbb513ec8ea0436d687801bc23e58106daa25
Artur-sgon <89609981+Artur-sgon@users.noreply.github.com>
2026-01-09 18:01:58 -0300

    Some changes

---
+++ parry_master_shared.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+    // 1. INJECT HEADER
+    const header = document.getElementById('shared-header');
+    if (header) {
+        header.innerHTML = `
+            <h1 class="text-white gold-glow">Parry-Master.net</h1>
+            <i class="text-white" style="background: rgba(0,0,0,0.5); padding: 2px 10px;">
+                ~ The Portal to the World of Parry Master ~
+            </i>
+        `;
+    }
+
+    // 2. INJECT SIDEBAR MENU
+    const sidebar = document.getElementById('shared-sidebar');
+    if (sidebar) {
+        sidebar.innerHTML = `
+            <b class="text-white">MAIN MENU</b>
+            <hr>
+            &raquo; <a href="parry_master.html">Home</a><br>
+            &raquo; <a href="#">Game mechanics</a><br>
+            &raquo; <a href="parry_master_lore.html">Parry Master Lore *NEW!</a><br>
+            &raquo; <a href="#">Characters</a><br>                    
+            &raquo; <a href="#">Development journal</a><br>
+            &raquo; <a href="#">The team</a><br>
+            &raquo; <a href="parry_master_ost.html">Soundtrack *NEW!</a><br>
+            <br>
+            <b class="text-white">AWARDS</b>
+            <hr>
+            <center>
+                <img src="https://web.archive.org/web/20091027083547im_/http://geocities.com/Tokyo/Pagoda/5461/coolsite.gif" width="80"><br>
+                <span class="text-grey">Links2Go Award Winner</span>
+            </center>
+        `;
+    }
+
+    // 3. INJECT FOOTER
+    const footer = document.getElementById('shared-footer');
+    if (footer) {
+        footer.innerHTML = `
+            SITE UPDATED: FRIDAY 9 JANUARY 2026 | <a href="mailto:webmaster@parry-master.net">CONTACT</a>
+            <br>
+            <img src="https://web.archive.org/web/20091027005003im_/http://geocities.com/TimesSquare/Labyrinth/8301/counter.gif" alt="Hit Counter" style="margin: 10px; filter: contrast(2) brightness(0.8);">
+            <br>
+            <b>YOU ARE VISITOR NUMBER #84,291</b>
+        `;
+    }
+});
